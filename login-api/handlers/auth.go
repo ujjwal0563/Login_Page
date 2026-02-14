482537a926f400b88d43542e71ddc93561e3fe06
Ujjwal Kesarwani <ujjwalkes19@gmail.com>
2026-02-14 23:16:26 +0530

    Increase context timeout duration for Signup function to improve performance

--- login-api/handlers/auth.go
+++ login-api/handlers/auth.go
@@ -93,7 +93,7 @@ func Signup(c *gin.Context) {
 		return
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
 	count, _ := db.UserCollection.CountDocuments(ctx, bson.M{

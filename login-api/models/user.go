961879abc9c2b90234e44aeb71f6191cc4a754ef
Ujjwal Kesarwani <ujjwalkes19@gmail.com>
2026-09-20 03:27:31 +0530

    feat: add password reset via Brevo email service and update input autofill styles

--- login-api/models/user.go
+++ login-api/models/user.go
@@ -9,10 +9,11 @@ import (
 type User struct {
 	ID               primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name             string             `bson:"name" json:"name"`
-	Surname          string             `bson:"surname" json:"surname"`
+	Surname          string             `bson:"surname,omitempty" json:"surname,omitempty"`
+	Username         string             `bson:"username,omitempty" json:"username,omitempty"`
 	Email            string             `bson:"email" json:"email"`
 	Password         string             `bson:"password" json:"-"`
-	PhoneNumber      string             `bson:"phone_number" json:"phone_number"`
+	PhoneNumber      string             `bson:"phone_number,omitempty" json:"phone_number,omitempty"`
    // Fields for password reset and OTP
 	ResetOTPHash     string             `bson:"reset_otp_hash,omitempty"`
 	OTPExpiresAt     time.Time          `bson:"otp_expires_at,omitempty"`

b99158989c64e69cfbc59efe89e641c6616cd98b
Ujjwal Kesarwani <ujjwalkes19@gmail.com>
2026-09-21 23:50:38 +0530

    feat: implement account blacklist checks and modularize signup logic

--- login-api/models/user.go
+++ login-api/models/user.go
@@ -7,13 +7,15 @@ import (
 )
 
 type User struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name        string             `bson:"name" json:"name"`
-	Username    string             `bson:"username,omitempty" json:"username,omitempty"`
-	Email       string             `bson:"email" json:"email"`
-	Password    string             `bson:"password" json:"-"`
-	PhoneNumber string             `bson:"phone_number,omitempty" json:"phone_number,omitempty"`
-	BlackList   bool               `bson:"blacklist" json:"blacklist"`
+	ID            primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name          string             `bson:"name" json:"name"`
+	Surname       string             `bson:"surname,omitempty" json:"surname,omitempty"`
+	Username      string             `bson:"username,omitempty" json:"username,omitempty"`
+	Email         string             `bson:"email" json:"email"`
+	Password      string             `bson:"password" json:"-"`
+	PhoneNumber   string             `bson:"phone_number,omitempty" json:"phone_number,omitempty"`
+	IsBlackListed bool               `bson:"is_blacklisted" json:"is_blacklisted"`
+	BlackList     bool               `bson:"blacklist" json:"blacklist"`
 	// Fields for password reset and OTP
 	ResetOTPHash    string    `bson:"reset_otp_hash,omitempty"`
 	OTPExpiresAt    time.Time `bson:"otp_expires_at,omitempty"`
